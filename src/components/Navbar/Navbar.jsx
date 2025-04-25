b0cc3449360ed03492070f4b64d467d25b266f91
Rashmi-kavindya <kavidissanayaka20@gmail.com>
2025-04-25 09:23:12 +0530

    add assets to src

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,55 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+// import styles from "./Navbar.module.css";
+// import { getImageUrl } from "../../utils";
+
+// export const Navbar = () => {
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   return (
+//     <nav className={styles.navbar}>
+//       <a className={styles.title} href="/">
+//         Portfolio
+//       </a>
+//       <div className={styles.menu}>
+//         <img
+//           className={styles.menuBtn}
+//           src={
+//             menuOpen
+//               ? getImageUrl("nav/closeIcon.png")
+//               : getImageUrl("nav/menuIcon.png")
+//           }
+//           alt="menu-button"
+//           onClick={() => setMenuOpen(!menuOpen)}
+//         />
+//         <ul
+//           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+//           onClick={() => setMenuOpen(false)}
+//         >
+//           <li>
+//             <a href="#about">About</a>
+//           </li>
+//           <li>
+//             <a href="#experience">Experience</a>
+//           </li>
+//           <li>
+//             <a href="#projects">Projects</a>
+//           </li>
+//           <li>
+//             <a href="#contact">Contact</a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import { getImageUrl } from "../../utils";
+
+// Import images directly
+import menuIcon from "../../assets/nav/menuIcon.png";
+import closeIcon from "../../assets/nav/closeIcon.png";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,11 +62,7 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
+          src={menuOpen ? closeIcon : menuIcon}
           alt="menu-button"
           onClick={() => setMenuOpen(!menuOpen)}
         />
