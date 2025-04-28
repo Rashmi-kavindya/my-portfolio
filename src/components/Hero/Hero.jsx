9a24aaece0d6ec888eba5d009a6bfa41cc214a18
Rashmi-kavindya <kavidissanayaka20@gmail.com>
2025-04-28 16:30:53 +0530

    update hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,44 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import heroImage from "../../assets/hero/heroImage.png";
+import { Typewriter } from "react-simple-typewriter";
 
 export const Hero = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Rashmi</h1>
-        <p className={styles.description}>
-          I'm a passionate and driven Computer Engineering undergraduate committed to continuous learning and growth.!
-        </p>
+
+        <h2 className={styles.description}>
+          I am a{" "}
+          <span style={{ color: "#0ef" }}> {/* you can style this color or remove */}
+            <Typewriter
+              words={[
+                "Full Stack Developer",
+                "AI/ML Enthusiast",
+                "Data Engineering Enthusiast"
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </h2>
+
         <a href="mailto:rashmikavindya40@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
+
       <img
         src={heroImage}
         alt="Hero image of me"
         className={styles.heroImg}
       />
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
